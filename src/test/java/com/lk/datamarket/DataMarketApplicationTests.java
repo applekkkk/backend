4c07605bab4b1e17a5e42574ca65999b3667792e
lk <15088797151@163.com>
2026-03-10 09:40:59 +0800

    init

---
+++ src/test/java/com/lk/datamarket/DataMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.datamarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
